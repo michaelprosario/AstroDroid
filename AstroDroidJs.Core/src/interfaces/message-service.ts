0573a23941ed989ec37f46699f5e46c92cb89e2d
michael_rosario <michael.rosario@designinteractive.net>
2021-02-06 22:57:41 -0500

    feat ( message service ) Initial commit

--- AstroDroidJs.Core/src/interfaces/message-service.ts
+++ AstroDroidJs.Core/src/interfaces/message-service.ts
@@ -1,6 +1,7 @@
-import { Subscriber } from "../entities/subscriber";
 import { INodeMessage } from "./node-message";
 import { INodeService } from "./node-service";
+import { Response } from "../responses/response";
+import { Subscriber } from "../entities/subscriber";
 
 export interface IMessageService {
     Subscribers: Subscriber[];
--- AstroDroidJs.Core/src/services/message-service.ts
+++ AstroDroidJs.Core/src/services/message-service.ts
@@ -5,16 +5,15 @@ import { INodeService } from "../interfaces/node-service";
 import { MessageValidator } from "../validators/message-validator";
 import { ResponseCode, Response } from "../responses/response";
 import { Subscriber } from "../entities/subscriber";
+import { Require } from "../helpers/requires";
 
-export class MessageService implements IMessageService
-{
+export class MessageService implements IMessageService {
     public Subscribers: Subscriber[] = [];
 
-    public SendMessage(message: INodeMessage): Response
-    {
+    public SendMessage(message: INodeMessage): Response {
         var messageValidator = new MessageValidator();
         var validationResults = messageValidator.validate(message);
-        if (!validationResults.isValid){
+        if (!validationResults.isValid) {
             let validationResponse = new Response();
             validationResponse.Code = ResponseCode.BadRequest;
             validationResponse.Message = "Validation errors on message";
@@ -24,31 +23,36 @@ export class MessageService implements IMessageService
 
         var topic = message.Topic;
         var subscribers = this.Subscribers.filter(s => s.Topic === topic);
-        for (let subscriber of subscribers) 
-          subscriber.NodeService.ReceiveMessage(message);
+        for (let subscriber of subscribers)
+            subscriber.NodeService.ReceiveMessage(message);
 
         return new Response();
     }
 
-    public Subscribe(topic: string, service: INodeService)
-    {
-        Guard.Against.NullOrEmpty(topic, nameof(topic));
-        Guard.Against.Null(service, nameof(service));
+    public Subscribe(topic: string, service: INodeService) {
+        Require.IsNotEmpty(topic, "topic");
+        Require.NotNull(service, "service");
 
-        var count = Subscribers
-            .Count(r => r.Topic.Equals(topic) && r.NodeService.NodeId.Equals(service.NodeId));
-
-        if (count > 0)
+        var collection = this.Subscribers.filter(r => r.Topic === topic && r.NodeService.NodeId === service.NodeId);
+        if (collection.length > 0)
             return;
 
-        Subscribers.Add(new Subscriber {Topic = topic, NodeService = service});
+        let subscriber = new Subscriber();
+        subscriber.Topic = topic;
+        subscriber.NodeService = service;
+        this.Subscribers.push(subscriber);
     }
 
-    public void Unsubscribe(string topic, INodeService service)
-    {
+    public Unsubscribe(topic: string, service: INodeService) {
+        Require.IsNotEmpty(topic, "topic");
+        Require.NotNull(service, "service");
+
         var subscriber =
-            Subscribers.First(r => r.Topic.Equals(topic) &&
-                                   r.NodeService.NodeId.Equals(service.NodeId));
-
-        Subscribers.Remove(subscriber);
+            this.Subscribers.find(r => r.Topic === topic &&
+                r.NodeService.NodeId === service.NodeId);
+        const index = this.Subscribers.indexOf(subscriber);
+        if (index > -1) {
+            this.Subscribers.splice(index, 1);
+        }
     }
+}
